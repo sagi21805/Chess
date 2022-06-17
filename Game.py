ef403f1fa44ad2f8bc53cab2a14d72796fef26b1
sagi21805 <105317735+sagi21805@users.noreply.github.com>
2022-06-17 12:40:01 +0300

    types acomplished

--- Game.py
+++ Game.py
@@ -1,3 +1,4 @@
+from re import I
 import pygame
 import numpy as np
 
@@ -40,53 +41,72 @@ class Game():
     BlackQueen = pygame.image.load("C:\\vscodeprojects\\python\\Chess\\Chess-1\\images\\Black Queen.png")
     
     self.Cells: list[list[Cell]] = []
-    for i in range(0, self.NumOfRows):
+    for n in range(0, self.NumOfRows):
       row: list[Cell] = []
-      for n in range(0, self.NumOfRows):
+      for i in range(0, self.NumOfRows):
         cell = Cell(n, i)
+        print([n, i])
         match i:
           case 0:
             match n:
               case 0 | 7:
                 self.surface.blit(BlackRook, (((n * self.Size_x / self.NumOfRows) + 15), (i * (self.Size_y) / self.NumOfRows)))
+                cell.type = "black rook"
               case 1 | 6:
                 self.surface.blit(BlackKnight, (((n * self.Size_x / self.NumOfRows) + 20), (i * (self.Size_y) / self.NumOfRows) - 5))
+                cell.type = "black knight"
               case 2 | 5:
                 self.surface.blit(BlackBishop, (((n * self.Size_x / self.NumOfRows) + 5), (i * (self.Size_y) / self.NumOfRows) + 5))
+                cell.type = "black bishop"
               case 3:
                 self.surface.blit(BlackQueen, (((n * self.Size_x / self.NumOfRows) + 10), (i * (self.Size_y) / self.NumOfRows)))
+                cell.type = "black queen"
               case 4:
                 self.surface.blit(BlackKing, (((n * self.Size_x / self.NumOfRows) + 10), (i * (self.Size_y) / self.NumOfRows)))
+                cell.type = "black king"
           case 1:
             self.surface.blit(BlackPawn, (((n * self.Size_x / self.NumOfRows) + 15), (i * (self.Size_y) / self.NumOfRows)))
+            cell.type = "black pawn"
           case 6:
             self.surface.blit(WhitePawn, (((n * self.Size_x / self.NumOfRows) + 15), (i * (self.Size_y) / self.NumOfRows)))
+            cell.type = "white pawn"
           case 7:
             match n:
               case 0 | 7:
                 self.surface.blit(WhiteRook, (((n * self.Size_x / self.NumOfRows) + 10), (i * (self.Size_y) / self.NumOfRows) - 5))
+                cell.type = "white rook"
               case 1 | 6:
                 self.surface.blit(WhiteKnight, (((n * self.Size_x / self.NumOfRows) + 10), (i * (self.Size_y) / self.NumOfRows) + 3))
+                cell.type = "white knight"
               case 2 | 5:
                 self.surface.blit(WhiteBishop, (((n * self.Size_x / self.NumOfRows) + 15), (i * (self.Size_y) / self.NumOfRows) + 5))
+                cell.type = "white bishop"
               case 3:
                 self.surface.blit(WhiteQueen, (((n * self.Size_x / self.NumOfRows) + 5), (i * (self.Size_y) / self.NumOfRows)))
+                cell.type = "white queen"
               case 4:
                 self.surface.blit(WhiteKing, (((n * self.Size_x / self.NumOfRows) + 5), (i * (self.Size_y) / self.NumOfRows)))
-
-
+                cell.type = "white king"
         row.append(cell)
-    self.Cells.append(row)
+      self.Cells.append(row)
       
     pygame.display.update()
-
+    
+  def ChangeUnits(self) -> list: 
+    pos = list(pygame.mouse.get_pos())
+    x = int(np.floor(int(pos[0]) / (self.Size_x / self.NumOfRows)))
+    y = int(np.floor(int(pos[1]) / (self.Size_y / self.NumOfRows)))
+    cords = []
+    cords.append(x)
+    cords.append(y)
+    return cords
       
   def start(self):
       while self.running:        
         ev = pygame.event.get()
         for event in ev:
             if event.type == pygame.MOUSEBUTTONDOWN:
-              pass
+              print(self.Cells[self.ChangeUnits()[0]][self.ChangeUnits()[1]].type)
 
             
             if event.type == pygame.QUIT:
@@ -98,6 +118,7 @@ class Cell():
     self.position_x = position_x
     self.position_y = position_y
     self.type = "empty"
+
     
 CHESS = Game(800, 700, 8)
 CHESS.start()
